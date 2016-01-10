aca2093bebac73940615e2b547e7b931bf8fa56d
shadeofgreene <brian@shadeofgreene.com>
2016-01-10 08:14:34 -0700

    Syncing data
    created generic function to update local db with server db for any
    collection/table. Still needs tested.

--- public/ajs-controllers/NgCustomerController.js
+++ public/ajs-controllers/NgCustomerController.js
@@ -1,237 +1,237 @@
-﻿app.controller('NgCustomerController', [
-	'$scope', 'Auth', 'Alert', '$location', '$http', 'GlobalUser', function ($scope, Auth, Alert, $location, $http, GlobalUser) {
-		helper.getOffices();
-		$scope.offices = [];
+app.controller('NgCustomerController', [
+	'$scope', 'Auth', 'Alert', '$location', '$http', 'GlobalUser',
+    function ($scope, Auth, Alert, $location, $http, GlobalUser) {
+        helper.getOffices();
+        $scope.offices = [];
 
-		$scope.initiateoffices = function() {
-			var offices = JSON.parse(localStorage.getItem('offices'));
-			$scope.offices = offices;
-		}
+        $scope.initiateoffices = function () {
+            var offices = JSON.parse(localStorage.getItem('offices'));
+            $scope.offices = offices;
+        }
 
-		$scope.addCustomer = {
-			customerName: null,
-			notes: null
-		}
+        $scope.addCustomer = {
+            customerName: null,
+            notes: null
+        }
 
-		$scope.displayOfficeModal = function(officeId) {
-			var customerOffice = _.find($scope.offices, function (office) {
-				debugger;
-				return office.officeId == officeId;
-			});
+        $scope.displayOfficeModal = function (officeId) {
+            var customerOffice = _.find($scope.offices, function (office) {
+                debugger;
+                return office.officeId == officeId;
+            });
 
-			$scope.displayOfficeObject = {
-				officeId: customerOffice.officeId,
-				officeName: customerOffice.officeName,
-				officeAddress1: customerOffice.officeAddress1,
-				officeAddress2: customerOffice.officeAddress2,
-				officeCity: customerOffice.officeCity,
-				officeState: customerOffice.officeState,
-				officeZip: customerOffice.officeZip,
-				officePhonePrimary: customerOffice.officePhonePrimary,
-				officePhoneSecondary: customerOffice.officePhoneSecondary,
-				customerId: customerOffice.customerId
-			}
+            $scope.displayOfficeObject = {
+                officeId: customerOffice.officeId,
+                officeName: customerOffice.officeName,
+                officeAddress1: customerOffice.officeAddress1,
+                officeAddress2: customerOffice.officeAddress2,
+                officeCity: customerOffice.officeCity,
+                officeState: customerOffice.officeState,
+                officeZip: customerOffice.officeZip,
+                officePhonePrimary: customerOffice.officePhonePrimary,
+                officePhoneSecondary: customerOffice.officePhoneSecondary,
+                customerId: customerOffice.customerId
+            }
 
-			$('#displayOfficeModal').modal('show');
-		}
+            $('#displayOfficeModal').modal('show');
+        }
 
-		$scope.displayOfficeFormDisabled = true;
-		$scope.toggleEnableDisplayOffice = function () {
-			debugger;
-			if ($scope.displayOfficeFormDisabled == true) {
-				$scope.displayOfficeFormDisabled = false;
-			} else {
-				$scope.displayOfficeFormDisabled = true;
-			}
-		}
+        $scope.displayOfficeFormDisabled = true;
+        $scope.toggleEnableDisplayOffice = function () {
+            debugger;
+            if ($scope.displayOfficeFormDisabled == true) {
+                $scope.displayOfficeFormDisabled = false;
+            } else {
+                $scope.displayOfficeFormDisabled = true;
+            }
+        }
 
-		$scope.addCustomerOffice = {
-			officeName: null,
-			officeAddress1: null,
-			officeAddress2: null,
-			officeCity: null,
-			officeState: null,
-			officeZip: null,
-			officePhonePrimary: null,
-			officePhoneSecondary: null,
-			customerId: null
-		};
+        $scope.addCustomerOffice = {
+            officeName: null,
+            officeAddress1: null,
+            officeAddress2: null,
+            officeCity: null,
+            officeState: null,
+            officeZip: null,
+            officePhonePrimary: null,
+            officePhoneSecondary: null,
+            customerId: null
+        };
 
-		$scope.customers = [];
+        $scope.customers = [];
 
-		// TODO
-		$scope.addNewCustomer = function () {
+        // TODO
+        $scope.addNewCustomer = function () {
 
-			Alert.showLoading('Loading...');
-			$http.post(helper.baseUrl + 'TicketSystem/CustomerView/SaveNewCustomer', JSON.stringify($scope.addCustomer)).success(function (customer) {
+            Alert.showLoading('Loading...');
+            $http.post(helper.baseUrl + 'TicketSystem/CustomerView/SaveNewCustomer', JSON.stringify($scope.addCustomer)).success(function (customer) {
 
-				Alert.hideLoading();
-				Alert.success('Customer saved successfully');
-				// prep the next view (add-customer-office.html)
-				helper.workingWith.NgCustomerController.customerName = customer.customerName;
-				helper.workingWith.NgCustomerController.customerId = customer.customerId;
+                Alert.hideLoading();
+                Alert.success('Customer saved successfully');
+                // prep the next view (add-customer-office.html)
+                helper.workingWith.NgCustomerController.customerName = customer.customerName;
+                helper.workingWith.NgCustomerController.customerId = customer.customerId;
 
-				//$scope.workingWith.customerName = customer.customerName;
-				//$scope.workingWith.customerId = customer.customerId;
-				$location.path('/add-customer-office');
-			}).error(function (err, error) {
+                //$scope.workingWith.customerName = customer.customerName;
+                //$scope.workingWith.customerId = customer.customerId;
+                $location.path('/add-customer-office');
+            }).error(function (err, error) {
 
-				Alert.hideLoading();
-				Alert.error('Failed to save customer');
-			});
-		}
+                Alert.hideLoading();
+                Alert.error('Failed to save customer');
+            });
+        }
 
-		$scope.addOfficeForThisCustomer = function (customerId, customerName) {
+        $scope.addOfficeForThisCustomer = function (customerId, customerName) {
 
-			helper.workingWith.NgCustomerController.customerName = customerName;
-			helper.workingWith.NgCustomerController.customerId = customerId;
-			$location.path('/add-customer-office');
-		}
+            helper.workingWith.NgCustomerController.customerName = customerName;
+            helper.workingWith.NgCustomerController.customerId = customerId;
+            $location.path('/add-customer-office');
+        }
 
-		$scope.updateOffice = function() {
-			Alert.showLoading('Loading...');
-			$http.post(helper.baseUrl + 'TicketSystem/CustomerView/UpdateOffice', JSON.stringify($scope.displayOfficeObject)).success(function (office) {
-				debugger;
-				Alert.hideLoading();
-				Alert.success('Office was updated successfully');
-			}).error(function (err, error) {
-				debugger;
-				Alert.hideLoading();
-				Alert.error('Failed to update office');
-			});
-		}
+        $scope.updateOffice = function () {
+            Alert.showLoading('Loading...');
+            $http.post(helper.baseUrl + 'TicketSystem/CustomerView/UpdateOffice', JSON.stringify($scope.displayOfficeObject)).success(function (office) {
+                debugger;
+                Alert.hideLoading();
+                Alert.success('Office was updated successfully');
+            }).error(function (err, error) {
+                debugger;
+                Alert.hideLoading();
+                Alert.error('Failed to update office');
+            });
+        }
 
-		// TODO
-		$scope.addNewOffice = function () {
+        // TODO
+        $scope.addNewOffice = function () {
 
-			Alert.showLoading('Loading...');
-			$http.post(helper.baseUrl + 'TicketSystem/CustomerView/SaveNewOffice', JSON.stringify($scope.addCustomerOffice)).success(function (office) {
+            Alert.showLoading('Loading...');
+            $http.post(helper.baseUrl + 'TicketSystem/CustomerView/SaveNewOffice', JSON.stringify($scope.addCustomerOffice)).success(function (office) {
 
-				Alert.hideLoading();
-				Alert.success('Office saved successfully');
-				// prep the next view "add-customer-office.html"
+                Alert.hideLoading();
+                Alert.success('Office saved successfully');
+                // prep the next view "add-customer-office.html"
 
-				helper.workingWith.NgCustomerController.customerName = null;
-				helper.workingWith.NgCustomerController.customerId = null;
-				//helper.getOffices();
-				//helper.getCustomers();
-				//$scope.getCustomers();
-				//$scope.initiateoffices();
-				$location.path('/customer-list');
-				
-				
-				//window.href = helper.baseUrl + '/ticketsystem/ticketsystem?#/customer-list';
-				//$('#customerListLink').trigger('click');
+                helper.workingWith.NgCustomerController.customerName = null;
+                helper.workingWith.NgCustomerController.customerId = null;
+                //helper.getOffices();
+                //helper.getCustomers();
+                //$scope.getCustomers();
+                //$scope.initiateoffices();
+                $location.path('/customer-list');
 
-			}).error(function (err, error) {
 
-				Alert.hideLoading();
-				Alert.error('Failed to save office');
-			});
-		}
+                //window.href = helper.baseUrl + '/ticketsystem/ticketsystem?#/customer-list';
+                //$('#customerListLink').trigger('click');
 
-		$scope.getOffices = function (customerId) {
-			debugger;
-			return _.filter($scope.offices, function (office) {
-				debugger;
-				return office.customerId === customerId;
-			});
-		}
+            }).error(function (err, error) {
 
-		$scope.getAllOffices = function () {
-			$http.get(helper.baseUrl + 'TicketSystem/CustomerView/GetAllOffices').success(function(offices) {
-				$scope.offices = offices;
-				$scope.getCustomers();
-			}).error(function(error, err) {
+                Alert.hideLoading();
+                Alert.error('Failed to save office');
+            });
+        }
 
-			});
-		}
+        $scope.getOffices = function (customerId) {
+            debugger;
+            return _.filter($scope.offices, function (office) {
+                debugger;
+                return office.customerId === customerId;
+            });
+        }
 
-		$scope.getCustomers = function (getCustomers) {
-			$http.get(helper.baseUrl + 'TicketSystem/CustomerView/GetCustomers').success(function (customers) {
-				$scope.offices = JSON.parse(localStorage.getItem('offices'));
-				$scope.customers = [];
+        $scope.getAllOffices = function () {
+            $http.get(helper.baseUrl + 'TicketSystem/CustomerView/GetAllOffices').success(function (offices) {
+                $scope.offices = offices;
+                $scope.getCustomers();
+            }).error(function (error, err) {
 
-				_.each(customers, function (customer) {
-					debugger;
-					customer.offices = [];
-					customer.offices = $scope.getOffices(customer.customerId);
-					debugger;
-					$scope.customers.push(customer);
-				});
-			}).error(function (error, err) {
+            });
+        }
 
-			});
-			
-		}
+        $scope.getCustomers = function (getCustomers) {
+            $http.get(helper.baseUrl + 'TicketSystem/CustomerView/GetCustomers').success(function (customers) {
+                $scope.offices = JSON.parse(localStorage.getItem('offices'));
+                $scope.customers = [];
 
-		$scope.getWorkingCustomerName = function () {
-			return helper.workingWith.NgCustomerController.customerName;
-		};
+                _.each(customers, function (customer) {
+                    debugger;
+                    customer.offices = [];
+                    customer.offices = $scope.getOffices(customer.customerId);
+                    debugger;
+                    $scope.customers.push(customer);
+                });
+            }).error(function (error, err) {
 
-		$scope.getWorkingCustomerId = function () {
-			$scope.addCustomerOffice.customerId = helper.workingWith.NgCustomerController.customerId;
-			return helper.workingWith.NgCustomerController.customerId;
-		};
+            });
+        }
 
-		$scope.makeCustomerInactive = function () {
-			debugger;
-			$('#confirmMakeCustomerInactiveModal').modal('hide');
-			Alert.showLoading('Loading...');
-			var postObject = {
-				Token: GlobalUser.getToken(),
-				Id: $scope.makeCustomerInactiveObject.customerId
-			}
-			$http.post(helper.baseUrl + 'TicketSystem/CustomerView/MakeCustomerInactive', JSON.stringify(postObject)).success(function() {
-				debugger;
-				Alert.hideLoading();
-				Alert.success('Customer has been removed');
-				$scope.getAllOffices();
-			}).error(function(error, err) {
-				debugger;
-				Alert.hideLoading();
-			});
-		}
+        $scope.getWorkingCustomerName = function () {
+            return helper.workingWith.NgCustomerController.customerName;
+        };
 
-		$scope.makeCustomerOfficeInactive = function () {
-			debugger;
-			$('#confirmMakeCustomerOfficeInactiveModal').modal('hide');
-			Alert.showLoading('Loading...');
-			var postObject = {
-				Token: GlobalUser.getToken(),
-				Id: $scope.makeCustomerOfficeInactiveObject.customerId
-			}
-			$http.post(helper.baseUrl + 'TicketSystem/CustomerView/MakeCustomerOfficeInactive', JSON.stringify(postObject)).success(function () {
-				debugger;
-				Alert.hideLoading();
-				Alert.success('Office has been removed');
-				$scope.getAllOffices();
-			}).error(function (error, err) {
-				debugger;
-				Alert.hideLoading();
-			});
-		}
+        $scope.getWorkingCustomerId = function () {
+            $scope.addCustomerOffice.customerId = helper.workingWith.NgCustomerController.customerId;
+            return helper.workingWith.NgCustomerController.customerId;
+        };
 
-		$scope.makeCustomerOfficeInactiveObject = {
-			customerOfficeId: null,
-			customerOfficeName: null
-		}
+        $scope.makeCustomerInactive = function () {
+            debugger;
+            $('#confirmMakeCustomerInactiveModal').modal('hide');
+            Alert.showLoading('Loading...');
+            var postObject = {
+                Token: GlobalUser.getToken(),
+                Id: $scope.makeCustomerInactiveObject.customerId
+            }
+            $http.post(helper.baseUrl + 'TicketSystem/CustomerView/MakeCustomerInactive', JSON.stringify(postObject)).success(function () {
+                debugger;
+                Alert.hideLoading();
+                Alert.success('Customer has been removed');
+                $scope.getAllOffices();
+            }).error(function (error, err) {
+                debugger;
+                Alert.hideLoading();
+            });
+        }
 
-		$scope.makeCustomerInactiveObject = {
-			customerId: null,
-			customerName: null
-		}
+        $scope.makeCustomerOfficeInactive = function () {
+            debugger;
+            $('#confirmMakeCustomerOfficeInactiveModal').modal('hide');
+            Alert.showLoading('Loading...');
+            var postObject = {
+                Token: GlobalUser.getToken(),
+                Id: $scope.makeCustomerOfficeInactiveObject.customerId
+            }
+            $http.post(helper.baseUrl + 'TicketSystem/CustomerView/MakeCustomerOfficeInactive', JSON.stringify(postObject)).success(function () {
+                debugger;
+                Alert.hideLoading();
+                Alert.success('Office has been removed');
+                $scope.getAllOffices();
+            }).error(function (error, err) {
+                debugger;
+                Alert.hideLoading();
+            });
+        }
 
-		$scope.makeCustomerInactiveModal = function(customerId, customerName) {
-			$scope.makeCustomerInactiveObject.customerId = customerId;
-			$scope.makeCustomerInactiveObject.customerName = customerName;
-			$('#confirmMakeCustomerInactiveModal').modal('show');
-		}
+        $scope.makeCustomerOfficeInactiveObject = {
+            customerOfficeId: null,
+            customerOfficeName: null
+        }
 
-		// execute this on view load
-		$scope.getAllOffices();
-		$scope.getWorkingCustomerId();
-		
-		//$scope.initiateoffices();
+        $scope.makeCustomerInactiveObject = {
+            customerId: null,
+            customerName: null
+        }
+
+        $scope.makeCustomerInactiveModal = function (customerId, customerName) {
+            $scope.makeCustomerInactiveObject.customerId = customerId;
+            $scope.makeCustomerInactiveObject.customerName = customerName;
+            $('#confirmMakeCustomerInactiveModal').modal('show');
+        }
+
+        // execute this on view load
+        $scope.getAllOffices();
+        $scope.getWorkingCustomerId();
+
+        //$scope.initiateoffices();
 	}
 ]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var querystring = require('querystring');
 var mongojs = require('mongojs');
 var collections = ['ticket', 'User'];
 var db = mongojs('MeTicket', ['Ticket', 'User']);
@@ -11,101 +12,207 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.post('/TryLoginAndGetUser', function (req, res) {
-	var user = req.body;
-	console.log(user);
-	if (user.userEmail && user.userPassword) {
-		db.User.findOne({
-			'userEmail': user.userEmail,
-			'userPassword': user.userPassword
-		}, function (err, checkUser) {
-			console.log(err);
-			console.log(checkUser);
-			if (err || !checkUser) {
-				console.log('user not found');
-				res.status(500).json({
-					error: 'Invalid credentials'
-				});
-			} else {
-				console.log('user was found');
-				res.json(checkUser);
-			}
-		});
-	}
+    var user = req.body;
+    console.log(user);
+    if (user.userEmail && user.userPassword) {
+        db.User.findOne({
+            'userEmail': user.userEmail,
+            'userPassword': user.userPassword
+        }, function (err, checkUser) {
+            if (err) {
+                console.log('error');
+                res.status(500).json({
+                    error: err
+                });
+            } else if (!checkUser) {
+                console.log('local user not found');
+                // try to get this user from server and add it to local db
+                var data = JSON.stringify({
+                    'username': user.userEmail,
+                    'password': user.userPassword
+                });
+                var options = {
+                    host: 'meticket.briangreenedev.com',
+                    path: '/TicketSystem/LoginView/TryLoginAndGetUser',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8',
+                        'Content-Length': Buffer.byteLength(data)
+                    }
+                };
+
+                var userString = '';
+                http.request(options, function (userRes) {
+                    userRes.setEncoding('utf8');
+                    userRes.on('data', function (chunk) {
+                        //console.log(chunk);
+                        userString += chunk;
+                    });
+                    userRes.on('end', function (data) {
+                        var userToSave = JSON.parse(userString);
+                        userToSave.userPassword = user.userPassword;
+                        console.log(userToSave);
+                        db.User.save(userToSave);
+                    });
+                }).write(data);
+
+            } else {
+                console.log('user was found');
+                res.json(checkUser);
+            }
+        });
+    }
 });
 
 app.post('/CreateTicketAndReturnTicket', function (req, res) {
-	var ticket = req.body;
-	console.log(ticket);
-	if (ticket) {
-		db.Ticket.save(ticket, function (err, ticket) {
-			if (!err) {
-				res.json(ticket);
-			} else {
-				res.json('Invalid');
-			}
-		});
-	}
+    var ticket = req.body;
+    console.log(ticket);
+    if (ticket) {
+        db.Ticket.save(ticket, function (err, ticket) {
+            if (!err) {
+                res.json(ticket);
+            } else {
+                res.json('Invalid');
+            }
+        });
+    }
 });
 
 app.get('/GetTickets', function (req, res) {
-	db.Ticket.find(function(err, tickets) {
-		if(!err) {
-			res.json(tickets);
-		} else {
-			res.status(500).json({error: 'Problem getting tickets'})
-		}
-	})
+    db.Ticket.find(function (err, tickets) {
+        if (!err) {
+            res.json(tickets);
+        } else {
+            res.status(500).json({
+                error: 'Problem getting tickets'
+            })
+        }
+    })
 });
 
 app.post('/SyncAllData', function (req, res) {
-	console.log('#############################################################')
-	console.log('#############################################################')
-	console.log('#############################################################')
-	console.log('#############################################################')
-	console.log('#############################################################')
-	var user = req.body;
-	if (user.userId) {
-		console.log('userId: ' + user.userId);
-		var userTickets = '';
+    console.log('#############################################################')
+    console.log('#############################################################')
+    console.log('#############################################################')
+    console.log('#############################################################')
+    console.log('#############################################################')
+    var user = req.body;
+    if (user.userId) {
+        console.log('userId: ' + user.userId);
 
-		var options = {
-			host: 'meticket.briangreenedev.com',
-			path: '/TicketSystem/TicketView/GetTickets',
-			method: 'GET'
-		};
-		http.get(options, function (ticketRes) {
-			ticketRes.setEncoding('utf8');
-			ticketRes.on('data', function (chunk) {
-				userTickets += chunk;
-			});
-			ticketRes.on('end', function (data) {
-				db.Ticket.find(function (err, userTicketsLocal) {
-					if (!err) {
-						// get tickets from server
-						var records = JSON.parse(userTickets);
-						var userTicketsServer = _.filter(records, function (record) {
-							return record.userId === user.userId;
-						});
+        ///////// GET TICKETS /////////////
+        ///////////////////////////////////
+        var userTickets = '';
+        var options = {
+            host: 'meticket.briangreenedev.com',
+            path: '/TicketSystem/TicketView/GetTickets',
+            method: 'GET'
+        };
+        http.get(options, function (ticketRes) {
+            ticketRes.setEncoding('utf8');
+            ticketRes.on('data', function (chunk) {
+                userTickets += chunk;
+            });
+            ticketRes.on('end', function (data) {
+                db.Ticket.find(function (err, userTicketsLocal) {
+                    if (!err) {
+                        // get tickets from server
+                        var records = JSON.parse(userTickets);
+                        var userTicketsServer = _.filter(records, function (record) {
+                            return record.userId === user.userId;
+                        });
 
-						// update local with server tickets
-						_.each(userTicketsServer, function (ticket) {
-							var existingTicket = _.find(userTicketsLocal, function (locTicket) {
-								return parseInt(ticket.ticketId) === parseInt(locTicket.ticketId);
-							})
-							if (!existingTicket) {
-								// save ticket to local
-								console.log(ticket);
-								db.Ticket.save(ticket);
-							}
-						});
-					}
-				});
-				//console.log(userTicketsServer);
-				res.json('finished');
-			})
-		});
-	}
+                        // update local with server tickets
+                        _.each(userTicketsServer, function (ticket) {
+                            var existingTicket = _.find(userTicketsLocal, function (locTicket) {
+                                return parseInt(ticket.ticketId) === parseInt(locTicket.ticketId);
+                            })
+                            if (!existingTicket) {
+                                // save ticket to local
+                                console.log(ticket);
+                                db.Ticket.save(ticket);
+                            }
+                        });
+                    }
+                });
+
+                ///////// GET CUSTOMERS ///////////
+                ///////////////////////////////////
+                var customerOptions = {
+                    host: 'meticket.briangreenedev.com',
+                    path: '/TicketSystem/CustomerView/GetCustomers',
+                    method: 'GET'
+                }
+                var customersString = '';
+                http.get(options, function (customerRes) {
+                    customerRes.setEncoding('utf8');
+                    customerRes.on('data', function (chunk) {
+                        customersString += chunk;
+                    });
+                    customerRes.on('end', function (data) {
+                        db.Customer.find(function (err, customersLocal) {
+                            if (!err) {
+                                // get customers from server
+                                var serverCustomers = JSON.parse(customersString);
+
+                                // update local with server customers
+                                _.each(serverCustomers, function (customer) {
+                                    var existingCustomer = _.find(customersLocal, function (localCustomer) {
+                                        return parseInt(ticket.ticketId) === parseInt(locTicket.ticketId);
+                                    })
+                                    if (!existingTicket) {
+                                        // save ticket to local
+                                        console.log(ticket);
+                                        db.Ticket.save(ticket);
+                                    }
+                                });
+                            }
+                        });
+
+                        ///////// GET CUSTOMERS ///////////
+                        ///////////////////////////////////
+                    })
+                });
+            })
+        });
+    }
 })
+
+function syncRecords(host, path, method, collection, collectionIdKey, callback) {
+    var options = {
+        host: host,
+        path: path,
+        method: method
+    };
+    var recordString = '';
+    http.get(options, function (serverRes) {
+        serverRes.setEncoding('utf8');
+        serverRes.on('data', function (chunk) {
+            recordString += chunk;
+        });
+        serverRes.on('end', function (data) {
+            db.getCollection(collection).find(function (err, localRecords) {
+                if (!err) {
+                    // get customers from server
+                    var serverRecords = JSON.parse(recordString);
+
+                    // update local with server customers
+                    _.each(serverRecords, function (serverRecord) {
+                        var existingRecord = _.find(localRecords, function (localRecord) {
+                            return parseInt(localRecord[collectionIdKey]) === parseInt(serverRecord[collectionIdKey]);
+                        })
+                        if (!existingRecord) {
+                            // save record to local
+                            console.log(ticket);
+                            db.getCollection(collection).save(serverRecord);
+                        }
+                    });
+                }
+            });
+            callback();
+        })
+    });
+}
 
 app.listen(3000);
 console.log('Server running on port 3000');
